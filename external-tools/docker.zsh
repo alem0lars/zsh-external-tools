be32eef94bfbad60f7ad9519bd4d456498196c28
Alessandro Molari <molari.alessandro@gmail.com>
2015-12-03 07:53:29 +0100

    Update docker.zsh

--- external-tools/docker.zsh
+++ external-tools/docker.zsh
@@ -34,6 +34,7 @@
 
 osx_dockertoolbox_start="/Applications/Docker/Docker Quickstart Terminal.app/Contents/Resources/Scripts/start.sh"
 
+
 if [ $commands[docker] ]; then # check if 'docker' is installed
   if [[ -f "${osx_dockertoolbox_start}" ]]; then
     eval "${osx_dockertoolbox_start}"
