82d1c7abbc6b825b9031ddf0bd7f0f737bca6675
Alessandro Molari <molari.alessandro@gmail.com>
2017-07-07 12:39:49 +0200

    update tmuxp completion (faster)

--- external-tools/tmuxp.zsh
+++ external-tools/tmuxp.zsh
@@ -32,8 +32,17 @@
 # }}} ##########################################################################
 
 
+# NOTE: To get the updated code of `_tmuxp_completion` run:
+#       _TMUXP_COMPLETE=source tmuxp
+_tmuxp_completion() {
+    COMPREPLY=( $( env COMP_WORDS="${COMP_WORDS[*]}" \
+                   COMP_CWORD=$COMP_CWORD \
+                   _TMUXP_COMPLETE=complete $1 ) )
+    return 0
+}
+
 if [ $commands[tmuxp] ]; then # check if 'tmuxp' is installed
   autoload -U bashcompinit
   bashcompinit
-  eval "$(_TMUXP_COMPLETE=source tmuxp)"
+  complete -F _tmuxp_completion -o default tmuxp;
 fi
