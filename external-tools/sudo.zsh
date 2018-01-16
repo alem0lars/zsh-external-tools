3515e499d0dd1b3c7a0bed2d0f49e860d32d0040
Alessandro Molari <molari.alessandro@gmail.com>
2018-01-16 16:49:52 +0100

    add sudo support

---
+++ external-tools/sudo.zsh
@@ -0,0 +1,56 @@
+# {{{ File header. #############################################################
+#                                                                              #
+# File informations:                                                           #
+# - Name:    external-tools/sudo.zsh                                           #
+# - Summary: Support for sudo.                                                 #
+# - Authors:                                                                   #
+#   - Alessandro Molari <molari.alessandro@gmail.com> (alem0lars)              #
+#                                                                              #
+# Project informations:                                                        #
+#   - Homepage:        https://github.com/alem0lars/zsh-external-tools         #
+#   - Getting started: see README.md in the project root folder                #
+#                                                                              #
+# License: Apache v2.0 (see below)                                             #
+#                                                                              #
+################################################################################
+#                                                                              #
+# Licensed to the Apache Software Foundation (ASF) under one more contributor  #
+# license agreements.  See the NOTICE file distributed with this work for      #
+# additional information regarding copyright ownership. The ASF licenses this  #
+# file to you under the Apache License, Version 2.0 (the "License"); you may   #
+# not use this file except in compliance with the License.                     #
+# You may obtain a copy of the License at                                      #
+#                                                                              #
+#   http://www.apache.org/licenses/LICENSE-2.0                                 #
+#                                                                              #
+# Unless required by applicable law or agreed to in writing, software          #
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT    #
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.             #
+# See the License for the specific language governing permissions and          #
+# limitations under the License.                                               #
+#                                                                              #
+# }}} ##########################################################################
+
+
+if [ $commands[sudo] ]; then
+  sudo-command-line() {
+    [[ -z $BUFFER ]] && zle up-history
+    if [[ $BUFFER == sudo\ * ]]; then
+      LBUFFER="${LBUFFER#sudo }"
+    elif [[ $BUFFER == $EDITOR\ * ]]; then
+      LBUFFER="${LBUFFER#$EDITOR }"
+      LBUFFER="sudoedit $LBUFFER"
+    elif [[ $BUFFER == sudoedit\ * ]]; then
+      LBUFFER="${LBUFFER#sudoedit }"
+      LBUFFER="$EDITOR $LBUFFER"
+    else
+      LBUFFER="sudo $LBUFFER"
+    fi
+  }
+
+  zle -N sudo-command-line
+  bindkey "\es" sudo-command-line
+fi
+
+
+# vim: set filetype=zsh :
